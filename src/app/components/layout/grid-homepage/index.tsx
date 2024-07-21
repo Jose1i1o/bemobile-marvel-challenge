b890fd603c2587e9c08a388c2b82e622f8da75b6
Jose Valenzuela <guadix88@gmail.com>
2024-07-21 14:57:06 +0200

    feat(box model): added container queries for desktop version https://github.com/Jose1i1o/bemobile-marvel-challenge/issues/6

--- src/app/components/layout/grid-homepage/index.tsx
+++ src/app/components/layout/grid-homepage/index.tsx
@@ -14,43 +14,45 @@ const GridLayout = async () => {
 
   return (
     <>
-      <section className="grid-layout">
-        {data ? (
-          data?.map(({ id, thumbnail: { path, extension }, name }: any) => (
-            <article
-              key={id}
-              className="card-layout__item"
-              role="gridcell"
-              tabIndex={id}
-            >
-              <Image
-                src={`${path}.${extension}`}
-                alt={name}
-                className="card-layout__item-image"
-                width={100}
-                height={100}
-                priority={true}
-                quality={100}
-              />
-              <div className="card-layout__item-footer">
-                <p className="card-layout__item-name" role="button">
-                  {name.toUpperCase()}
-                </p>
-                <svg
-                  className="card-layout__item-favourite"
-                  role="button"
-                  aria-labelledby="heart-icon-title"
-                >
-                  <use href="/sprite.svg#heart-icon" />
-                </svg>
-              </div>
-            </article>
-          ))
-        ) : (
-          <section className="error-container">
-            <p className="error-message">Zap! No heroes available!</p>
-          </section>
-        )}
+      <section className="container">
+        <span className="grid-layout">
+          {data ? (
+            data?.map(({ id, thumbnail: { path, extension }, name }: any) => (
+              <article
+                key={id}
+                className="card-layout__item"
+                role="gridcell"
+                tabIndex={id}
+              >
+                <Image
+                  src={`${path}.${extension}`}
+                  alt={name}
+                  className="card-layout__item-image"
+                  width={100}
+                  height={100}
+                  priority={true}
+                  quality={100}
+                />
+                <div className="card-layout__item-footer">
+                  <p className="card-layout__item-name" role="button">
+                    {name.toUpperCase()}
+                  </p>
+                  <svg
+                    className="card-layout__item-favourite"
+                    role="button"
+                    aria-labelledby="heart-icon-title"
+                  >
+                    <use href="/sprite.svg#heart-icon" />
+                  </svg>
+                </div>
+              </article>
+            ))
+          ) : (
+            <section className="error-container">
+              <p className="error-message">Zap! No heroes available!</p>
+            </section>
+          )}
+        </span>
       </section>
     </>
   )
